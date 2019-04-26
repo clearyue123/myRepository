028de2f6ff7f4f8e4d3ef7389c9309a09a533b67
clearyue123 <clearyue123@sina.com>
2019-04-26 14:08:02 +0800

    代码提交

--- jingjingshop/target/jingjingshop-0.0.1-SNAPSHOT/js/controller/userController.js
+++ jingjingshop/target/jingjingshop-0.0.1-SNAPSHOT/js/controller/userController.js
@@ -3,33 +3,11 @@ app.controller("userController",function($scope,$controller,$http,userService){
 	// AngularJS中的继承:伪继承
 	$controller('baseController',{$scope:$scope});
 
-	$scope.findAll = function(){
-		// 向后台发送请求:
-		userService.findAll().success(function(response){
-			$scope.list = response;
-		});
-	}
-	
-	$scope.findByPage = function(page,rows){
-		// 向后台发送请求获取数据:
-		userService.findByPage(page,rows).success(function(response){
-			$scope.paginationConf.totalItems = response.total;
-			$scope.list = response.rows;
-		});
-	}
-	
-	$scope.findById = function(id){
-		brandService.findById(id).success(function(response){
-			$scope.entity = response;
-		});
-	}
-	
     $scope.searchEntity={};
-	
 	// 假设定义一个查询的实体：searchEntity
 	$scope.search = function(page,rows){
 		// 向后台发送请求获取数据:
-		brandService.search(page,rows,$scope.searchEntity).success(function(response){
+		userService.search(page,rows,$scope.searchEntity).success(function(response){
 			$scope.paginationConf.totalItems = response.total;
 			$scope.list = response.rows;
 		});
